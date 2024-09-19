b3d709338ae6fdd1b8664fb52063100f23fd319c
jomoartz <johnjohananmogbolu@gmail.com>
2024-09-19 04:21:51 +0100

    my first commit

---
+++ e2e/App.test.js
@@ -0,0 +1,13 @@
+describe('my detox tested app', ()=>{
+    beforeAll(async () => {
+        await device.launchApp();
+      });
+    
+    beforeEach(async () => {
+        await device.launchApp();
+    });
+
+    it('Displays a welcome screen')
+},
+
+);
